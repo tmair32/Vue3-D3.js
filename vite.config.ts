39f1595a8379ba30229080c00637b3e5a59906db
tmair32 <tmair32@gmail.com>
2022-08-16 10:23:45 +0900

    :wrench: Setting Vite Plugin Pages
    Reference
    [Vite-Plugin-Pages](https://github.com/hannoeru/vite-plugin-pages)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from "vite";
 import eslintPlugin from "vite-plugin-eslint";
-
+import Pages from "vite-plugin-pages";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 
+import { resolve } from "path";
+import { readFileSync } from "fs";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslintPlugin(), vue(), WindiCSS()],
+  plugins: [
+    Pages({
+      pagesDir: [
+        {
+          dir: "src/pages",
+          baseRoute: "/",
+        },
+      ],
+      extensions: ["vue"],
+      extendRoute(route) {
+        // Get inspired from anthony fu"s personal website
+        // https://github.com/antfu/antfu.me
+        const path = resolve(__dirname, route.component.slice(1));
+        const md = readFileSync(path, "utf-8");
+      },
+    }),
+    eslintPlugin(),
+    vue(),
+    WindiCSS(),
+  ],
 });
